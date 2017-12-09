e80adfb46514c54c7e8ef89b23760f8d74e61005
MeadowsMustangs <33379325+MeadowsMustangs@users.noreply.github.com>
2017-12-09 10:29:09 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+function login() {
+
+
+
+
+   var username = document.getElementById('username').value;
+
+
+   var password = document.getElementById('password').value;
+
+
+  
+
+
+   if (username === 'hello' && password === 'coolpeeps101') {
+
+
+       document.getElementById('error').classList.add('hide');
+
+
+       window.location.href = 'home.html';
+
+
+   } else {
+
+
+       document.getElementById('error').classList.remove('hide');
+
+
+   }
+
+
+}
